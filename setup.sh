1ceaf0532e08e1b412fc169f1d79e450287902de
Mentor Palokaj <mentor@palokaj.co>
2018-08-12 12:01:01 +0200

    Exit on error

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+# Exit if erroe
+set -e
+
 sudo apt update
 sudo apt upgrade -y
 
 # Install zsh and highlighting
 sudo apt install zsh -y
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
 mkdir -p ~/.oh-my-zsh/custom/themes/
 curl -o ~/.oh-my-zsh/custom/themes/agnoster-newline.zsh-theme https://gist.githubusercontent.com/nweddle/e456229c0a773c32d37b/raw/b4fef3b4a113677e47ab08cc98bd8cbc71d1a4dc/agnoster-newline.zsh-theme
