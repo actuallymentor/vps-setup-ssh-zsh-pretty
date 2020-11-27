8f96842e5f998ffc1b556c5ec01aaecb5d80a7ad
Mentor Palokaj <mentor@palokaj.co>
2020-11-27 15:45:44 +0100

    Manual zsh install for better control

--- 03-zsh.sh
+++ 03-zsh.sh
@@ -7,19 +7,32 @@ chsh -s `which zsh`
 # Recyclable zsh install function for use here and in the nonroot user section
 function installOhMyZSH() {
 
-	ZSH=/home/$1/.oh-my-zsh
+	userhome=$( eval echo "~$1" )
+	ZSH=$userhome/.oh-my-zsh
 	echo "Installing Oh My ZSH as $1 in $ZSH"
+	mv $userhome/.zshrc $userhome/.zshrc.bak || echo "no existing config"
 
 	# install as current user or as suplied user
-	sudo -u $1 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+	echo "Starting oh my zsh install"
+	sudo -u $1 sh -c "git clone https://github.com/ohmyzsh/ohmyzsh.git $ZSH"
+	echo "Done installing oh my zsh"
+	wait
 
+	echo "Creating custom theme folder"
 	mkdir -p $ZSH/custom/themes/
 	curl -o $ZSH/custom/themes/agnoster-newline.zsh-theme https://gist.githubusercontent.com/nweddle/e456229c0a773c32d37b/raw/b4fef3b4a113677e47ab08cc98bd8cbc71d1a4dc/agnoster-newline.zsh-theme
-	echo '
-	ZSH_THEME="agnoster-newline"
+	
+	echo -e "
+	ZSH=$ZSH
+	ZSH_THEME='agnoster-newline'
 	plugins=(git)
 	source $ZSH/oh-my-zsh.sh
-	' >> /home/$1/.zshrc
+	" > $userhome/.zshrc
+
+
+	cat $userhome/.zshrc
+	source $userhome/.zshrc
+
 
 }
 
