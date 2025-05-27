6ef9bc15a713acd62e78d36e8dd9efcd55594f6b
Mentor <mentor@palokaj.co>
2025-05-27 13:52:30 +0200

    ✨ make tmp ramdisisk

--- 04-swap.sh
+++ 04-swap.sh
@@ -27,3 +27,17 @@ vm.vfs_cache_pressure=10" | sudo tee -a /etc/sysctl.conf
 sudo cp /etc/fstab /etc/fstab.bak
 sudo sed -i '/swap/d' /etc/fstab
 echo "$swaploc none swap sw 0 0" | sudo tee -a /etc/fstab
+
+# Make /tmp use tmpfs
+sudo cp /etc/fstab /etc/fstab.bak
+sudo sed -i '/tmpfs/d' /etc/fstab
+
+# Make tmpfs with half the RAM size
+tmpfs_size=$(echo $(( $(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE) / (1024 * 1024) / 2 )))
+echo "tmpfs /tmp tmpfs defaults,noatime,nosuid,nodev,mode=1777,size=${tmpfs_size}M 0 0" | sudo tee -a /etc/fstab
+
+# Create /tmp directory if it does not exist
+sudo mkdir -p /tmp
+
+# Mount all
+sudo mount -a
