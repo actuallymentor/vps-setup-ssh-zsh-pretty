b7425f4e0830d3a5ddd51a381ae817503e5bfb1b
Mentor <mentor@palokaj.co>
2025-10-21 15:44:47 +0200

    newlines for ssh pubkeys

--- 00-ssh.sh
+++ 00-ssh.sh
@@ -16,6 +16,7 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
+echo "" >> ~/.ssh/authorized_keys
 cat ./key.pub >> ~/.ssh/authorized_keys
 sudo sed -i 's/#\{0,1\}AuthorizedKeysFile/AuthorizedKeysFile/g' /etc/ssh/sshd_config
 sudo sed -i 's/ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
